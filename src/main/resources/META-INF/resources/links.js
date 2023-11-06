01c75130a70132142541721358c4a4c44bdf7ae8
AllysomFernando <allysom.renczenczen18@gmail.com>
2023-11-06 17:17:04 -0300

    Adicionando mudanças no javascript

--- src/main/resources/META-INF/resources/links.js
+++ src/main/resources/META-INF/resources/links.js
@@ -1,4 +1,4 @@
-// Inicializando variáveis
+// Evento de clique para os botões
 let inputBuffer = "";
 let operator = "";
 let firstInput = "";
@@ -11,22 +11,20 @@ const buttons = document.getElementById("botoes");
 function updateDisplay() {
     display.value = inputBuffer;
 }
-
-// Evento de clique para os botões
 buttons.addEventListener("click", async function(event) {
     const value = event.target.innerText;
 
     if (!isNaN(value) || value === ".") {
         // Se o valor é um número ou um ponto
         inputBuffer += value;
-    } else if (["/", "*","+", "-"].includes(value)) {
+    } else if (["/", "*", "+", "-"].includes(value)) {
         // Se o valor é um operador
         if (firstInput === "") {
             firstInput = inputBuffer;
             inputBuffer = "";
             operator = value;
         }
-    }else if (value === "=") {
+    } else if (value === "=") {
         // Se o valor é o sinal de igual
         if (firstInput !== "") {
             const response = await fetch(`http://localhost:8080/calculate?first=${encodeURIComponent(firstInput)}&second=${encodeURIComponent(inputBuffer)}&operator=${encodeURIComponent(operator)}`);
@@ -38,10 +36,33 @@ buttons.addEventListener("click", async function(event) {
                 alert("Error: " + await response.text());
             }
         }
-    }else if(value === "CE"){
+    } else if (value === "CE") {
+        // Se o valor é "CE" (Limpar)
         inputBuffer = "";
         firstInput = "";
         operator = "";
+    } else if (value === "sin" || value === "cos" || value === "tan" || value === "sqrt") {
+        // Se o valor é uma operação científica
+        if (inputBuffer !== "") {
+            const response = await fetch(`http://localhost:8080/scientific?value=${encodeURIComponent(inputBuffer)}&operation=${encodeURIComponent(value)}`);
+            if (response.ok) {
+                inputBuffer = await response.text();
+            } else {
+                alert("Error: " + await response.text());
+            }
+        }
+    } else if (value === "^") {
+        // Se o valor é "^" (potenciação)
+        if (firstInput !== "") {
+            const response = await fetch(`http://localhost:8080/scientific?base=${encodeURIComponent(firstInput)}&exponent=${encodeURIComponent(inputBuffer)}`);
+            if (response.ok) {
+                inputBuffer = await response.text();
+                firstInput = "";
+                operator = "";
+            } else {
+                alert("Error: " + await response.text());
+            }
+        }
     }
 
     updateDisplay();
